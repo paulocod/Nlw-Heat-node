e63248ac9bb541999040a61e23cc4104a69d5fce
PauloCampos <paulo4-0@outlook.com>
2021-10-23 18:14:59 -0300

    create the app and the first migrations

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { router } from "./routes";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`usuario se conectou ${socket.id}`);
+});
+
+app.use(router);
+
+app.get("/github", (request, response) => {
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
+
+serverHttp.listen(4000, () => console.log("rodando"));
